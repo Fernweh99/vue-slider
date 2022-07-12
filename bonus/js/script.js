4b4d127a9a2199bc360ae8be4d5769a13a6e3890
Fernweh99 <daniele.dimento@gmail.com>
2022-07-12 15:48:44 +0200

    finish bonus 1

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,10 +1,8 @@
 /*
-Le caratteristiche minime richieste sono:
-- Immagine grande visibile quando attiva
-- Lista di Thumbnails in basso
-- Anche nelle thumbnails dobbiamo avere una classe active corrispondente all'immagine attiva in quel momento (lo stile è a vostra discrezione)
-- Al click sulle freccette l'immagine principale deve cambiare (e la thumbnail corrispondente deve avere la classe active con conseguente effetto visivo)
-- Implementare il ciclo infinito: se sono sulla prima immagine e clicco prev, devo ricominciare dall'ultima. Se sono sull'ultima e clicco next, devo ripartire dalla prima.
+Bonus:
+1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente (questo richiederà qualcosa che non abbiamo visto)
+3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce (questo richiederà degli eventi che non abbiamo visto)
 */
 
 const app = new Vue({
@@ -60,6 +58,9 @@ const app = new Vue({
       } else {
         this.currentIndex--;
       }
+    },
+    goToRespectiveImage(i) {
+      this.currentIndex = i;
     }
   }
 })
